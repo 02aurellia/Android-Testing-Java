70f8ea4deefc7903731560da4fa8bb435464466d
Aurellia <aurelliazza@gmail.com>
2024-02-26 11:07:18 +0700

    delete unused import

--- demo/src/test/java/cucumber/stepDef/LoginStep.java
+++ demo/src/test/java/cucumber/stepDef/LoginStep.java
@@ -2,15 +2,9 @@ package cucumber.stepDef;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.Duration;
-import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Point;
-import org.openqa.selenium.interactions.Pause;
-import org.openqa.selenium.interactions.PointerInput;
-import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import config.env;
